2ccd171f33b088b9e0a1c33ac22e9ec0156ba617
Josh R. Dunlavy <dunlavy@users.noreply.github.com>
2020-01-27 15:32:48 -0800

    feature: remove message box and play async

--- LlamaDex/ViewModels/LlamaRamaDexViewModel.cs
+++ LlamaDex/ViewModels/LlamaRamaDexViewModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.IO;
 using System.Media;
 using System.Runtime.CompilerServices;
-using System.Windows;
 
 using LlamaRamaDex.DataAccess;
 using LlamaRamaDex.Models.Interfaces;
@@ -24,7 +23,7 @@ namespace LlamaRamaDex.ViewModels {
 			set {
 				_selectedLlama = value;
 				_raisePropertyChanged(nameof(SelectedLlama));
-				_makeLlamaScream(value);
+				_makeLlamaScream();
 			}
 		}
 
@@ -45,10 +44,6 @@ namespace LlamaRamaDex.ViewModels {
 		private void _raisePropertyChanged([CallerMemberName] string propertyName = null) =>
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-		private void _makeLlamaScream(ILlama llama) {
-			new SoundPlayer(Path.Combine("WAV", "GoatScream.wav")).PlaySync();
-
-			MessageBox.Show("You just made " + llama.Name + " scream, bruh", "Screaming llama...", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-		}
+		private void _makeLlamaScream() => new SoundPlayer(Path.Combine("WAV", "GoatScream.wav")).Play();
 	}
 }
