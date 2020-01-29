d0efafd99cb6d0e0605afb91a1b04b6fc49b24ce
Josh R. Dunlavy <dunlavy@users.noreply.github.com>
2020-01-29 14:21:07 -0800

    refactor: constructor injection for page on MainWindow

--- LlamaDex/MainWindow.xaml.cs
+++ LlamaDex/MainWindow.xaml.cs
@@ -1,7 +1,32 @@
-﻿using System.Windows.Navigation;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
 
 namespace LlamaRamaDex {
 	public partial class MainWindow:NavigationWindow {
-		public MainWindow() => InitializeComponent();
+		private Page _compositedLlamaDexPage;
+
+		public MainWindow(Page configuredLlamaPage) => _initializePageComponent(configuredLlamaPage);
+
+		private void _navigate(object sender, RoutedEventArgs e) {
+			Hyperlink referringHyperlink = (Hyperlink)sender;
+
+			if (referringHyperlink.Name == nameof(Llama)) {
+				// "Llama" was the hyperlink clicked by the user, but now pages are using
+				// constructor injection... obviously no longer a parameterless constructor!
+				// We reassert some of the XAML functionality here; use navigation service
+				// with our injected page!
+
+				NavigationFrame.NavigationService.Navigate(_compositedLlamaDexPage);
+			}
+		}
+
+		private void _initializePageComponent(Page llamaDexPage) {
+			InitializeComponent();
+			_compositedLlamaDexPage = llamaDexPage;
+			// start to have llama page loaded (done before via XAML declaration)
+			NavigationFrame.NavigationService.Navigate(_compositedLlamaDexPage);
+		}
 	}
 }
